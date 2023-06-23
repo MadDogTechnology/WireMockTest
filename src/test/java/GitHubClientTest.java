70ec6335cc9a3d036fc543bf06a0ec5fa8bb00c6
oaydas23 <oaydas@resolutebi.com>
2023-06-23 12:04:36 -0400

    Update GitHubClientTest.java

--- src/test/java/GitHubClientTest.java
+++ src/test/java/GitHubClientTest.java
@@ -7,13 +7,17 @@ public class GitHubClientTest {
     
     GitHubClient githubclient = new GitHubClient();
 
+    // Tests whether WireMock server starts up without issues
     @Before
     public void setup () {
         githubclient.startServer();
     }
 
+    // Tests whether methods of githubclient object perform without compiling errors
     @Test
     public void test () {
         githubclient.replayApiRequest();
     }
+
+    // Will continue to work on tests to see if methods are performing as expected
 }
